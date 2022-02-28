5ef54ae24d49fa272468bca0a981d2269d590342
Jason Stajich <jasonstajich.phd@gmail.com>
2022-02-28 11:20:42 -0800

    add protein results

---
+++ comparative/BUSCO/pipeline/11_BUSCO_pep_euk.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+#SBATCH --nodes 1 --ntasks 24 --mem 32G -p short --out logs/busco_pep_euk.%a.log -J buscoEUK
+
+# for augustus training
+#export AUGUSTUS_CONFIG_PATH=/bigdata/stajichlab/shared/pkg/augustus/3.3/config
+# set to a local dir to avoid permission issues and pollution in global
+export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
+
+module load workspace/scratch
+
+CPU=${SLURM_CPUS_ON_NODE}
+N=${SLURM_ARRAY_TASK_ID}
+if [ ! $CPU ]; then
+     CPU=2
+fi
+
+if [ ! $N ]; then
+    N=$1
+    if [ ! $N ]; then
+        echo "Need an array id or cmdline val for the job"
+        exit
+    fi
+fi
+if [ -z ${SLURM_ARRAY_JOB_ID} ]; then
+	SLURM_ARRAY_JOB_ID=$$
+fi
+GENOMEFOLDER=pep
+LINEAGE=eukaryota_odb10
+OUTFOLDER=BUSCO_pep_euk
+mkdir -p $OUTFOLDER
+SAMPLEFILE=pep_samples.csv
+IFS=,
+cat $SAMPLEFILE | sed -n ${N}p | while read FILE
+do
+	BASE=$(basename $FILE .fasta)
+	GENOMEFILE=$(realpath $GENOMEFOLDER/$FILE)
+	
+	if [ -d "$OUTFOLDER/${BASE}" ];  then
+	    echo "Already have run $NAME in folder busco - do you need to delete it to rerun?"
+	    exit
+	else
+	    module load busco/5.2.2
+	    busco -m protein -l $LINEAGE -c $CPU -o ${BASE} --out_path ${OUTFOLDER} --offline --in $GENOMEFILE --download_path $BUSCO_LINEAGES --tar
+	fi
+done
