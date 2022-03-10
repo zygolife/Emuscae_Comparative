dacdb3ac53b027e06d4803d38ad3bd96a88cb8d3
Brian Lovett <lovettbr@umd.edu>
2022-03-10 11:23:59 -0500

    CAZY update
    CAZY analysis included

--- comparative/domains/scripts/EMU Comparative Domains.R
+++ comparative/domains/scripts/EMU Comparative Domains.R
@@ -35,13 +35,6 @@ merops=merops.raw %>%
   separate(Name, into=c("Strain", "Accession"), sep="\\|")
 
 merops.counts=merops %>%
-  mutate(Genome=recode(Genome,"Entomophthora_muscae_UCB.v3"="EMU",
-                       "Entomophaga_maimaiga_ARSEF_7190.v1"="EMA",
-                       "Strongwellsea_castrans_DrSc_Trinity"="SCA",
-                       "Pandora_formicae_Trinity"="PFO",
-                       "Zoophthora_radicans_ATCC_208865"="ZRA",
-                       "Conidiobolus_thromboides__ARSEF_4968"="CTH",
-                       "Conidiobolus_coronatus_NRRL_28638.Conco1.v1"="CCO")) %>%
   group_by(Genome, MERNUM) %>%
   summarize(Count=n_distinct(Accession)) %>%
   group_by(Genome) %>%
@@ -78,7 +71,6 @@ mer.uplt
 grid.text("MEROPS UpSet Plot", x = 0.65, y=0.95, gp=gpar(fontsize=20))
 
 #MEROPS enrichment
-#Enrichment function
 enrich = function(dat, var='DOMAIN', group="Genome", thresh=0.01){
   out=data.frame()
   vars=unique(dat[[var]])
@@ -98,7 +90,7 @@ enrich = function(dat, var='DOMAIN', group="Genome", thresh=0.01){
     out=rbind(out, test2)
   }
   return(out)
-}
+} #Enrichment function
 
 merops.res=enrich(merops.dat, var="MERNUM", group="Genome")
 
@@ -253,5 +245,170 @@ mer4=ggplotGrob(mer.plt4)
 layout2 <- rbind(c(1,2))
 
 grid.arrange(mer4, mer3, layout_matrix=layout2, widths=c(0.4, 0.6))
+
+####CAZY analysis####
+cazy.files=data.frame(names=str_replace(list.files("CAZY/"), ".tsv", "")) %>%
+  filter(names %in% chosen) %>%
+  mutate(loc=paste("CAZY/", names, ".tsv", sep="")) %>%
+  mutate(source=as.character(1:length(loc)), Genome=as.factor(c("CCO", "CTH", "EMA", "EMU", "PFA", "SCA", "ZRA")))
+
+cazy.raw=cazy.files$loc %>% map_dfr(read.delim, .id="source", header=F) %>%
+  left_join(cazy.files %>% select(source, Genome)) %>%
+  select(-source)
+
+cazy=cazy.raw %>%
+  select(V3, V1, Genome) %>%
+  rename(Name=`V3`, CAZY=`V1`) %>%
+  separate(Name, into=c("Strain", "Accession"), sep="\\|") %>%
+  mutate(CAZY=str_replace_all(CAZY, ".hmm", ""))
+
+cazy.counts=cazy %>%
+  group_by(Genome, CAZY) %>%
+  summarize(Count=n_distinct(Accession)) %>%
+  group_by(Genome) %>%
+  mutate(n=sum(Count)) %>%
+  group_by(CAZY) %>%
+  mutate(n_Genomes=length(unique(Genome)), Genomes=toString(unique(Genome)))
+
+cazy.composition=cazy.counts %>%
+  group_by(Genome) %>%
+  summarize(n=sum(n_Genomes), CAZYs=list(unique(CAZY)))
+
+cazy.unique=cazy.counts %>%
+  filter(n_Genomes==1) %>%
+  group_by(Genome) %>%
+  summarize(n=n_distinct(CAZY), cazy=list(unique(CAZY)))
+
+cazy.dat=cazy.counts %>%
+  filter(n_Genomes>1) %>%
+  select(-n_Genomes) 
+
+cazy.phylo=levels(as.factor(cazy.dat$Genome))[c(1, 2, 5, 6, 7, 3, 4)]
+
+upset.dat=cazy.composition$CAZYs
+names(upset.dat)=cazy.composition$Genome
+
+cazy.uplt=upset(fromList(upset.dat), sets=cazy.phylo, mb.ratio = c(0.55, 0.45), order.by = "freq", keep.order = TRUE)
+
+cazy.uplt
+grid.text("CAZY UpSet Plot",x = 0.65, y=0.95, gp=gpar(fontsize=20))
+
+#CAZY enrichment
+cazy.res=enrich(cazy.dat, var="CAZY", group="Genome")
+
+cazy.res2 = cazy.res %>%
+  group_by(group1, group2) %>%
+  summarize(Domains=toString(Domain)) %>%
+  pivot_wider(id_cols=group1, names_from=group2, values_from=Domains)
+
+cazy.res3=cazy.res2[match(rev(cazy.phylo), cazy.res2$group1),]
+
+res4=cazy.res3[,match(c("group1", intersect(rev(cazy.phylo), colnames(cazy.res3))), colnames(cazy.res3)),]
+
+cazy.res.stat=data.frame(Genome=c(cazy.res$group1, cazy.res$group2), Domain=c(cazy.res$Domain, cazy.res$Domain)) %>%
+  group_by(Genome, Domain) %>%
+  summarize(sig_n=length(Domain))
+
+cazy.meta=cazy.dat %>%
+  select(CAZY, Genome, Count) %>%
+  rename(Domain="CAZY") %>%
+  group_by(Domain) %>%
+  mutate(avg=median(Count), Fold=ifelse((Count+avg)>0, Count/avg, 0),
+         Direction=ifelse(Fold>1 | Fold==Inf, "Up", ifelse(Fold==1 | Fold==0, ifelse(Count<avg, "Down", "Equal"), "Down"))) %>%
+  mutate(Fold=ifelse(Direction=="Down", 1/Fold, Fold))
+
+cazy.res.stat.meta=inner_join(cazy.res.stat, cazy.meta)
+
+cazy.res.stat.meta$Genome=as.factor(cazy.res.stat.meta$Genome)
+
+#CAZY clustering
+cazy.dist=cazy.res.stat.meta %>%
+  select(Domain, Genome, sig_n) %>%
+  pivot_wider(id_cols="Domain", values_from=sig_n, names_from=Genome) %>% mutate(
+    across(where(is.numeric), ~replace_na(.x, 0))
+  )
+
+cazy.dist2=as.matrix(cazy.dist[2:length(cazy.dist)])
+names(cazy.dist2)=cazy.dist$Domain
+
+cazy.hclust=hclust(d = dist(cazy.dist2))
+
+cazy.hclust2 <- data.frame(Domain=names(cazy.dist2),
+                           cluster=cutree(cazy.hclust, length(cazy.dist$Domain)))
+
+cazy.clust=left_join(cazy.res.stat.meta, cazy.hclust2)
+
+cazy.clust$Genome=as.factor(cazy.clust$Genome)
+cazy.clust$Genome=factor(cazy.clust$Genome, levels=levels(cazy.clust$Genome)[match(cazy.phylo, levels(cazy.clust$Genome))])
+
+cazy.list = unique(subset(cazy.res.stat.meta, cazy.res.stat.meta$Genome=="EMU")$Domain)
+
+Emus.cazy.stat=cazy.clust %>%
+  filter(Domain %in% cazy.list) %>%
+  filter(Genome=="EMU") %>%
+  ungroup() %>%
+  select(Domain, Direction) %>%
+  distinct() %>%
+  rename("Emus.status"="Direction") %>%
+  mutate(Emus.status=recode(Emus.status, "Down"="Down in\nEMU", "Up"="Up in\nEMU"))
+
+Emus.cazy.clust=cazy.clust %>%
+  left_join(Emus.cazy.stat) %>%
+  mutate(Emus.status=replace_na(Emus.status, "Missing")) %>%
+  filter(Emus.status!="Equal" & Emus.status!="Missing") %>%
+  mutate(Direction=as.factor(Direction)) %>%
+  mutate(Direction=factor(Direction, levels=levels(Direction)[c(2, 1, 3)])) 
+
+#CAZY visualization
+cazy.plt=ggplot(Emus.cazy.clust, aes(y=reorder(Domain, cluster), x=Genome, color=Direction, size=sig_n))+geom_point(alpha=0.8)+
+  theme+geom_stripes(inherit.aes=F, aes(y=reorder(Domain, cluster)), odd = "#33333333", even = "#00000000")+theme(axis.text.x = element_text(angle = 90, vjust=0.5, hjust=0.95))+
+  theme(axis.text.y=element_text(size=15), axis.text.x=element_text(size=10))+theme(legend.position="right", strip.text.y = element_text(size = 12, color = "black", face = "bold"),strip.background = element_rect(color="black", fill="white", size=1.5, linetype="solid"))+ylab("CAZY")+facet_grid(~Emus.status, scales="free_x", space="free_x")+scale_color_manual(values=c("#127852", "#8B8588", "#FF0000"))+labs(color="Direction\nvs. median", size="Significant\nDifferences")+coord_flip()+guides(colour = guide_legend(override.aes = list(size=3), order=1))
+
+cazy.plt
+
+cazy.plt2=ggplot(subset(Emus.cazy.clust, Genome=="EMU"), aes(x=Count, y=reorder(Domain, cluster), fill=Fold))+geom_col()+
+  theme+geom_stripes(odd = "#33333333", even = "#00000000")+
+  theme(axis.text.y=element_text(size=15), axis.text.x=element_blank(), axis.title.x=element_blank(), axis.ticks.x = element_blank(), strip.text.y = element_text(size = 12, color = "black", face = "bold"),strip.background = element_rect(color="black", fill="white", size=1.5, linetype="solid"))+theme(legend.position="right")+xlab("Count for EMU only")+facet_grid(~Emus.status, scales="free_x", space="free_x")+coord_flip()+labs(fill="Fold vs.\nmedian")+scale_fill_viridis_c()
+
+cazy.plt2
+
+Emus.cazy.missing=cazy.clust %>%
+  left_join(Emus.cazy.stat) %>%
+  mutate(Emus.status=as.character(Emus.status)) %>%
+  mutate(Emus.status=replace_na(Emus.status, "Missing from EMU")) %>%
+  filter(Emus.status=="Missing from EMU") %>%
+  mutate(Direction=as.factor(Direction)) %>%
+  mutate(Direction=factor(Direction, levels=levels(Direction)[c(2, 1, 3)]))
+
+Emus.cazy.unique=cazy.counts %>%
+  filter(CAZY %in% unlist(cazy.unique[cazy.unique$Genome=="EMU",]$cazy))
+
+cazy.plt3=ggplot(Emus.cazy.missing, aes(y=reorder(Domain, rev(cluster)), x=Genome, color=Direction, size=as.factor(sig_n)))+geom_point(alpha=0.8)+
+  theme+theme(axis.text.x = element_text(angle = 90, vjust=0.5))+
+  theme(axis.text.x=element_text(size=20), axis.text.y=element_text(size=10))+theme(legend.position="right")+xlab("Genome")+scale_color_manual(values=c("#127852", "#8B8588", "#FF0000"))+labs(size="Significant\nDifferences")+geom_stripes(inherit.aes=F, aes(y=reorder(Domain, rev(cluster))), odd = "#33333333", even = "#00000000")+scale_y_discrete(limits = rev(unique(Emus.cazy.missing$Domain)))+scale_x_discrete(limits = rev(cazy.phylo))+ylab("CAZY")+ggtitle("Missing from EMU")+theme(plot.title = element_text(hjust = 0.5))+guides(colour = guide_legend(override.aes = list(size=3), order=1))
+
+cazy.plt3
+
+cazy.plt4=ggplot(Emus.cazy.unique, aes(x=CAZY, y=Count))+geom_col(fill="black")+
+  theme+theme(axis.text.x = element_text(angle = 90, vjust=0.5))+xlab("CAZY")+scale_x_discrete(limits = rev(Emus.cazy.unique$CAZY))+scale_y_continuous(breaks=c(0:2))+coord_flip()+ggtitle("Unique to EMU")+theme(plot.title = element_text(hjust = 0.5))
+
+cazy.plt4
+
+cazy.1=ggplotGrob(cazy.plt)
+cazy.2=ggplotGrob(cazy.plt2)
+
+maxWidth <- unit.pmax(cazy.1$widths, cazy.2$widths)
+
+cazy.1$widths= maxWidth
+cazy.2$widths= maxWidth
+
+grid.arrange(cazy.1, cazy.2, layout_matrix=layout1, heights=c(0.4, 0.6))
+
+cazy.3=ggplotGrob(cazy.plt3)
+cazy.4=ggplotGrob(cazy.plt4)
+
+grid.arrange(cazy.4, cazy.3, layout_matrix=layout2, widths=c(0.4, 0.6))
+
+
 
 
