d78f7c9f058874f04c88d46ab130802714425600
Brian Lovett <lovettbr@umd.edu>
2022-03-18 15:34:22 -0400

    Fixed counting bugs

--- comparative/Telomeres/scripts/Telomere script.R
+++ comparative/Telomeres/scripts/Telomere script.R
@@ -20,15 +20,15 @@ GTGTTTTGTTCCCCCTTGATCCACTCTTTCTCTATGTTGCAAGCTTTGGCGCCATCAACCTTGGAGGGGTTAGTTACAAG
 teloresults2 <- teloresults %>%
   select(-source) %>%
   mutate(Sequence=toupper(Sequence)) %>%
+  mutate(Scaffold=paste(Genome, Scaffold, sep="_")) %>%
   mutate(len=str_length(Sequence), count1=str_count(Sequence, telo_motif1), count2=str_count(Sequence, telo_motif2), sum_motifs=count1+count2, both_motifs=(count1*count2)>0) %>%
+  group_by(Scaffold) %>%
   pivot_wider(names_from="Direction", values_from="Direction", values_fn=length) %>%
-  mutate(across(c("forward", "reverse"), ~replace_na(.x, 0)),
-         both=ifelse(forward==1 & reverse==1, 1, 0))
+  mutate(across(c("forward", "reverse"), ~replace_na(.x, 0))) %>%
+  group_by(Scaffold) %>%
+  mutate(both=ifelse(sum(forward, na.rm=T)==1 & sum(reverse, na.rm=T)==1, T, F))
 
 teloresult3 <- teloresults2 %>%
   group_by(Genome) %>%
-  summarize(FWD=sum(forward), REV=sum(reverse), Both=sum(both), Total_w_telo=FWD+REV-Both,
+  summarize(FWD=sum(forward), REV=sum(reverse), Both=sum(both)/2, Total_w_telo=FWD+REV-Both,
             min.motifs=min(sum_motifs), med.motifs=median(sum_motifs), max.motifs=max(sum_motifs), sd.motifs=sd(sum_motifs))
-
-
-
