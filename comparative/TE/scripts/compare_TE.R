943df64bd3a68ef9fd586d48558fd29de33551bd
Brian Lovett <lovettbr@umd.edu>
2022-03-30 18:07:38 -0400

    Updated DNA repeat landscape plot
    Updated formatting and to display percent of genome along y axis

--- comparative/TE/scripts/compare_TE.R
+++ comparative/TE/scripts/compare_TE.R
@@ -25,7 +25,14 @@ genomenames <- tibble::rowid_to_column(data.frame(genomenames),"source")
 genome_sizes <- indexes %>% map_dfr(read_tsv, .id="source",col_names = c("Scaffold","length","offset","linebases","linewidth"), col_types=c("ciii")) %>%
     group_by(source) %>% summarize(totallen = sum(length)) %>% type_convert(col_types="ii")
 
-genome_sizes <- genome_sizes %>% left_join(genomenames,by="source") %>% select(genomenames,totallen) %>% rename(genomename=genomenames,length=totallen) 
+genome_sizes <- genome_sizes %>% left_join(genomenames,by="source") %>% select(genomenames,totallen) %>% rename(genomename=genomenames,length=totallen)  %>%
+  rename("species"=genomename) %>%
+  mutate(species=str_replace(species, "Entomophaga_maimaiga_var_ARSEF_7190", "EMA"),
+         species=str_replace(species, "Entomophthora_muscae_UCB", "EMU"),
+         species=str_replace(species, "Massospora_cicadina_MCPNR19", "MCI"),
+         species=str_replace(species, "Zoophthora_radicans_ATCC_208865", "ZRA"),
+         species=str_replace(species, "Conidiobolus_thromboides_FSU_785.Conth1", "CTH"),
+         species=factor(species, levels=c("EMU", "EMA", "MCI", "ZRA", "CTH")))
 
 tefiles2=tefiles %>%
   mutate(species=tefiles,
@@ -79,28 +86,28 @@ TEcounts3 <- TEcounts2 %>%
 
 theme = theme_bw()+theme(text = element_text(size=15), axis.title.x = element_text(size=20), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15), title = element_text(size=20), legend.title = element_text(size=20), legend.text = element_text(size=15), strip.background = element_rect(color="black", fill="white", size=1.5, linetype="solid"))
 
-p<-ggplot(TEcounts3,aes(x=species,y=percent,fill=superfamily)) + theme + geom_bar(position="dodge",stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+ ylab("Percent")+ xlab("")+
+p<-ggplot(TEcounts3 %>% filter(!is.na(superfamily)),aes(x=species,y=percent,fill=superfamily)) + theme + geom_bar(position="dodge",stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+ ylab("Percent")+ xlab("")+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 p
 #ggsave("TE_species.pdf",p,width=10,height=12)
 
-p<-ggplot(TEcounts3 %>% filter(superfamily=="DNA",percent>0.001),aes(x=species,y=percent,fill=subfamily))+ theme+ geom_bar(position="dodge",stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+
+p_DNA<-ggplot(TEcounts3 %>% filter(superfamily=="DNA",percent>0.001),aes(x=species,y=percent,fill=subfamily))+ theme+ geom_bar(position=position_dodge2(preserve = "single"),stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   ylab("Percent")+ xlab("")+facet_wrap(~superfamily)
-p
+p_DNA
 #ggsave("TE_DNA_species.pdf",p,width=10,height=12)
 
-p<-ggplot(TEcounts3 %>% filter(superfamily=="LTR"),aes(x=species,y=percent,fill=subfamily)) + theme + geom_bar(position="dodge",stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+
+p_LTR<-ggplot(TEcounts3 %>% filter(superfamily=="LTR"),aes(x=species,y=percent,fill=subfamily)) + theme + geom_bar(position=position_dodge2(preserve = "single"),stat="identity") + scale_fill_viridis_d() + scale_y_continuous(labels=percent)+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   ylab("Percent")+ xlab("")+facet_wrap(~superfamily)
-p
+p_LTR
 #ggsave("TE_LTR_species.pdf",p,width=10,height=12)
 
-p<-ggplot(TEcounts3 %>% filter(superfamily=="LINE"),aes(x=species,y=percent,fill=subfamily)) + theme+ geom_bar(position="dodge",stat="identity") + 
+p_LINE<-ggplot(TEcounts3 %>% filter(superfamily=="LINE"),aes(x=species,y=percent,fill=subfamily)) + theme+ geom_bar(position="dodge",stat="identity") + 
   scale_fill_viridis_d() + scale_y_continuous(labels=percent)+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   ylab("Percent")+ xlab("")+facet_wrap(~superfamily)
-p
+p_LINE
 #ggsave("TE_LINE_species.pdf",p,width=10,height=12)
 
 #write_csv(tbl,"RM_scores.csv.bz2")
@@ -117,18 +124,31 @@ class_tbl <- repland %>% map_dfr(read.csv, .id = "source",sep="") %>% mutate(sou
 thresh=0.01
 
 DNA_comb=class_tbl %>%
-  filter(Type=="DNA" & Count>0) %>%
+  filter(Type=="DNA") %>%
   arrange(Type, Class) %>%
-  group_by(Class, Genome) %>%
-  mutate(sm=sum(Count)) %>%
+  group_by(Class) %>%
+  mutate(Class_sm=sum(Count)) %>%
   ungroup() %>%
-  mutate(tot=sum(Count), per=sm/tot) %>%
-  mutate(Class=ifelse(per<thresh, "Other", Class))
+  mutate(Genome_sm=sum(Count), Genome_per=Class_sm/Genome_sm) %>%
+  mutate(Class=ifelse(Genome_per<thresh, "Other", Class)) %>%
+  select(-Genome_per, -Genome_sm) %>%
+  left_join(genome_sizes %>% rename("Genome"=species)) %>%
+  group_by(Genome, Div, Diff, Class) %>%
+  summarize(genome_percent=sum(Count)/length) %>%
+  ungroup() %>%
+  mutate(Class=as.factor(Class)) %>%
+  mutate(Class=factor(Class, levels=levels(Class)[4:1])) %>%
+  mutate(Class=recode_factor(Class, `DNA.TcMar.ISRm11`="TcMar-ISRm11",
+                             `DNA.Sola.2`="Sola-2",
+                             `DNA.MULE.MuDR`="MULE-MuDR")) %>%
+  arrange(Genome, Class, Div) %>%
+  group_by(Genome, Class)
+  
 
-plt1=ggplot(DNA_comb, aes(x=Div, y=Count, color=Class))+geom_point(size=1)+geom_line(size=1)+theme+scale_color_viridis_d()+facet_wrap(~Genome)+theme(axis.text.x=element_text(angle=90, size=15))+xlab("Div")
+plt1=ggplot(DNA_comb, aes(x=Div, y=genome_percent, color=Class))+geom_point(size=3)+geom_line(size=2)+theme+scale_color_viridis_d()+facet_wrap(~Genome)+theme(axis.text.x=element_text(angle=90, size=20), axis.text.y=element_text(size=20), axis.title.y=element_text(size=25), axis.title.x=element_text(size=25), strip.text=element_text(size=22), legend.text=element_text(size=22))+xlab("Div")+ylab("Percent of Genome")+theme(legend.position=c(0.83, 0.25), legend.title=element_text(size=22, face="bold"), plot.title = element_text(size=25, face="bold", hjust = 0.5))+ggtitle("Landscapes of DNA Repeat Elements")+scale_y_continuous(labels=percent)
 
 plt1
 
-plt2=ggplot(DNA_comb, aes(x=Div, y=Diff, color=Class))+geom_line(size=1)+theme+scale_color_viridis_d()+facet_wrap(~Genome)+theme(axis.text.x=element_text(angle=90, size=15))+xlab("Div")
+plt2=ggplot(DNA_comb, aes(x=Div, y=Diff, color=Class))+geom_line(size=2)+theme+scale_color_viridis_d()+facet_wrap(~Genome)+theme(axis.text.x=element_text(angle=90, size=20), axis.text.y=element_text(size=20), strip.text=element_text(size=22), legend.text=element_text(size=22))+xlab("Div")+theme(legend.position=c(0.8, 0.2))
 
 plt2
